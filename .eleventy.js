4d127a3a6d8db66efb283731a1a9447968f78f2d
Jim O'Donnell <jim@zooniverse.org>
2020-04-09 10:10:24 +0100

    Base elevenpack install

---
+++ .eleventy.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+
+const manifestPath = path.resolve(__dirname, "dist", "assets", "manifest.json");
+const manifest = JSON.parse(
+  fs.readFileSync(manifestPath, { encoding: "utf8" })
+);
+
+module.exports = function(eleventyConfig) {
+  // Layout aliases make templates more portable.
+  eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+
+  // Adds a universal shortcode to return the URL to a webpack asset. In Nunjack templates:
+  // {% webpackAsset 'main.js' %} or {% webpackAsset 'main.css' %}
+  eleventyConfig.addShortcode("webpackAsset", function(name) {
+    if (!manifest[name]) {
+      throw new Error(`The asset ${name} does not exist in ${manifestPath}`);
+    }
+    return manifest[name];
+  });
+
+  // Copy all images directly to dist.
+  eleventyConfig.addPassthroughCopy({ "src/img": "img" });
+
+  // Copy external dependencies to dist.
+  eleventyConfig.addPassthroughCopy({ "src/vendor": "vendor" });
+
+  // Reload the page every time the JS/CSS are changed.
+  eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] });
+
+  // A debug utility.
+  eleventyConfig.addFilter("dump", obj => {
+    return util.inspect(obj);
+  });
+
+  eleventyConfig.addFilter("log", obj => {
+    console.log(obj);
+  });
+
+  return {
+    dir: {
+      input: "src/site",
+      includes: "_includes", // relative to dir.input
+      output: "dist",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true,
+  };
+};
