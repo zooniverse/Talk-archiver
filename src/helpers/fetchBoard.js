d2f49dd5114c49187420dd86e31bbec9cf142e8b
Jim O'Donnell <jim@zooniverse.org>
2020-04-12 11:07:01 +0100

    Add API module Handle API requests in a common module. Cache responses by URL for speed. Use Object.assign() rather than directly modifying API responses.

--- src/helpers/fetchBoard.js
+++ src/helpers/fetchBoard.js
@@ -1,34 +1,25 @@
-const fetch = require('node-fetch');
+const API = require('./api')
 const fetchDiscussion = require('./fetchDiscussion');
 
-async function fetchPage(host, project, boardID, page) {
-  console.log('fetching board page', boardID, page)
-  const response = await fetch(
-      `${host}/_ouroboros_api/projects/${project}/talk/boards/${boardID}?page=${page}`,
-    {
-      headers: { Accept: "application/json" }
-    }
-  );
-  const data = await response.json();
+async function fetchPage(boardID, page) {
+  const data = await API.get(`boards/${boardID}?page=${page}`);
   return data;
 }
 
-module.exports = async function fetchBoard(host, project, board, pages) {
-  let discussions = [];
+module.exports = async function fetchBoard(board, pages) {
+  const discussions = [];
   console.log('build discussions', board.zooniverse_id, board.discussions, pages)
 
   for (let page = 1; page <= pages; page++) {
-    const pageData = await fetchPage(host, project, board.zooniverse_id, page);
-    discussions = discussions.concat(pageData.discussions);
+    const pageData = await fetchPage(board.zooniverse_id, page);
+
+    for (discussion of pageData.discussions) {
+      const pages = Math.ceil(discussion.comments_count / 10) || 1;
+      const fullDiscussion = await fetchDiscussion(board, discussion, pages);
+      discussions.push(fullDiscussion);
+    }
   }
 
-  for (discussion of discussions) {
-    const pages = Math.ceil(discussion.comments_count / 10) || 1;
-    const fullDiscussion = await fetchDiscussion(host, project, board, discussion, pages);
-    discussion = fullDiscussion;
-  }
-
-  board.discussions = discussions;
-  return board;
+  return Object.assign({}, board, { discussions });
 }
 
--- src/helpers/fetchBoards.js
+++ src/helpers/fetchBoards.js
@@ -1,8 +1,5 @@
-const fetch = require('node-fetch');
+const API = require('./api');
 const fetchBoard = require('./fetchBoard');
-
-const HOST = 'https://www.penguinwatch.org'
-const PROJECT = 'illustratedlife'
 
 const CATEGORIES = [
   'chat',
@@ -12,7 +9,7 @@ const CATEGORIES = [
 async function discussionBoard(board) {
   if ( board.zooniverse_id && board.discussions ) {
     const pages = Math.ceil(board.discussions / 10) || 1
-    const fullBoard = await fetchBoard(HOST, PROJECT, board, pages);
+    const fullBoard = await fetchBoard(board, pages);
     console.log('board discussions', board.zooniverse_id, board.discussions.length)
     return fullBoard
   }
@@ -20,13 +17,7 @@ async function discussionBoard(board) {
 }
 
 module.exports = async function fetchBoards() {
-  const response = await fetch(
-      `${HOST}/_ouroboros_api/projects/${PROJECT}/talk/boards`,
-    {
-      headers: { Accept: "application/json" }
-    }
-  );
-  const boards = await response.json();
+  const boards = await API.get('boards/');
   for (category of CATEGORIES) {
     console.log(category)
     const categoryBoards = await Promise.all(boards[category].map(discussionBoard));
