8e978c0a46d441d3720eae880e6ba1526a114e2e
Jim O'Donnell <jim@zooniverse.org>
2020-04-16 16:41:03 +0100

    Get subjects from the collections store Request subjects after the collection store has been built. Wait on collections before building subjects.

--- src/site/_data/subjects.js
+++ src/site/_data/subjects.js
@@ -1,6 +1,18 @@
+const API = require('../../helpers/api');
 const store = require('../../helpers/store');
+const fetchUserCollections = require('./userCollections');
 
 module.exports = async function fetchSubjects() {
-  const { subjects } = await store;
-  return subjects;
+  const collections = await fetchUserCollections();
+
+  for (collection of Object.values(collections)) {
+    for (subject of collection.subjects) {
+      if (!store.subjects[subject.zooniverse_id]) {
+        const fullSubject = await API.get(`subjects/${subject.zooniverse_id}`)
+        store.subjects[subject.zooniverse_id] = fullSubject;
+      }
+    }
+  }
+
+  return store.subjects;
 }
--- src/site/_data/userCollections.js
+++ src/site/_data/userCollections.js
@@ -10,12 +10,6 @@ module.exports = async function fetchCollections() {
         if (!store.userCollections[collection.zooniverse_id]) {
           const userCollection = await API.get(`collections/${collection.zooniverse_id}`)
           store.userCollections[collection.zooniverse_id] = userCollection;
-          for (subject of userCollection.subjects) {
-            if (!store.subjects[subject.zooniverse_id]) {
-              const fullSubject = await API.get(`subjects/${subject.zooniverse_id}`)
-              store.subjects[subject.zooniverse_id] = fullSubject;
-            }
-          }
         }
       }
     } catch (e) {
