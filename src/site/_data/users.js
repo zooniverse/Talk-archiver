e10992d0201f8e4b2bcc4551f6ef7327202efb04
Jim O'Donnell <jim@zooniverse.org>
2020-04-17 13:55:27 +0100

    Page user requests Collections are paged on the user request, so run paged requests until we're sure we've got all of them.

--- src/site/_data/users.js
+++ src/site/_data/users.js
@@ -1,6 +1,24 @@
 const API = require('../../helpers/api');
 const store = require('../../helpers/store');
 
+
+async function fetchPage(name, page) {
+  const data = await API.get(`users/${name}?type=my_collections&page=${page}`);
+  return data;
+}
+
+async function listCollections(user) {
+  let userCollections = [];
+  for (let page = 2; page <= 100; page++) {
+    const { my_collections } = await fetchPage(user.name, page);
+    if (my_collections.length === 0) {
+      return userCollections
+    }
+    userCollections = userCollections.concat(my_collections);
+  }
+  return userCollections
+}
+
 module.exports = async function fetchUsers() {
   const { discussions } = await store;
 
@@ -9,6 +27,8 @@ module.exports = async function fetchUsers() {
       const { user_name } = comment;
       if (!store.users[user_name]) {
         const user = await API.get(`users/${user_name}`);
+        const userCollections = await listCollections(user);
+        user.my_collections.push(...userCollections);
         store.users[user_name] = user;
       }
     }
