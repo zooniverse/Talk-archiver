24602b1b80590e46831859f9096da146f37b4cda
Jim O'Donnell <jim@zooniverse.org>
2020-05-05 18:16:22 +0100

    Add requestQueue to API Add a request queue to the API client. Add api.batchedGet(urls) which will resolve with the data for an array of urls.

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { default: RequestQueue, END_EVENT, ITEM_EVENT } = require('limited-request-queue');
 
 const HOST = 'https://www.penguinwatch.org';
 const PROJECT = 'illustratedlife';
@@ -6,22 +7,39 @@ const CACHING = true;
 
 const apiCache = {};
 
-async function get(url) {
-  console.log(`Request: ${url}`);
-  if (CACHING && apiCache[url]) {
-    console.log('using cache')
-    return apiCache[url];
-  }
-
+const requestQueue = new RequestQueue({
+  maxSockets: 10,
+  maxSocketsPerHost: 10
+})
+.on(ITEM_EVENT, async function handleURL(url, promise, done) {
   const response = await fetch(
-      `${HOST}/_ouroboros_api/projects/${PROJECT}/talk/${url}`,
+      url,
     {
       headers: { Accept: "application/json" }
     }
   );
   const data = await response.json();
   apiCache[url] = Object.assign({}, data);
-  return data;
+  promise.resolve(apiCache[url]);
+  done();
+})
+
+async function getURL(url) {
+  if (CACHING && apiCache[url]) {
+    console.log('using cache')
+    return apiCache[url];
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    requestQueue.enqueue(new URL(`${HOST}/_ouroboros_api/projects/${PROJECT}/talk/${url}`), { resolve });
+  });
+  return promise;
 }
 
-module.exports = { get };
+async function batchedGet(urls) {
+  console.log(`Batch Request: ${urls[0]}`);
+  const promises = urls.map(getURL)
+  return Promise.all(promises);
+}
+
+module.exports = { batchedGet, get: getURL };
