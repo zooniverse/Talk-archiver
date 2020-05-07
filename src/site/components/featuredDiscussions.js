78699caae1aee965f88dc2c29f90ef49d599e074
Jim O'Donnell <jim@zooniverse.org>
2020-05-07 10:04:20 +0100

    Apply Zooniverse layout and branding (#15)
    Install the zooniverse theme.
    Update Tailwind CSS.
    Apply zooniverse brand colours, font, typography and spacing.
    Add landmark regions to pages and make page structure consistent across discussions, subjects, collections, tags and user profiles.
    Fix a bug where subject comments displayed the build date instead of the comment date.

--- src/site/components/featuredDiscussions.js
+++ src/site/components/featuredDiscussions.js
@@ -2,15 +2,18 @@ const moment = require('moment');
 
 function discussionSummary(discussion) {
   const lastPost = moment(discussion.last_comment.created_at).format('LLL');
+  const posts = discussion.comments.length || discussion.comments;
   return `
     <p>
       <a href="/boards/${ discussion.board._id }/discussions/${ discussion.zooniverse_id }">
         ${ discussion.title }
       </a>
     </p>
-    <p>${ discussion.comments } posts / ${ discussion.users } participants</p>
+    <p>${ posts } posts / ${ discussion.users } participants</p>
     <p>
-      Last post ${ lastPost } by
+      Last post
+      <time datetime="${discussion.last_comment.created_at}">${ lastPost }</time>
+      by
       <a href="/users/${discussion.last_comment.user_name}">
         ${ discussion.last_comment.user_name }
       </a>
@@ -21,7 +24,7 @@ function discussionSummary(discussion) {
 
 function listItem(discussion) {
   return `
-    <li>
+    <li class="discussion-preview">
       ${discussionSummary(discussion)}
     </li>
   `
@@ -29,7 +32,7 @@ function listItem(discussion) {
 
 module.exports = function featuredDiscussions(discussions) {
   return `
-    <ul>
+    <ul class="discussion-list">
       ${discussions.map(listItem).join('\n')}
     </ul>
   `
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+const zooTheme = require('@zooniverse/grommet-theme');
+const { colors } = zooTheme.global;
+
 module.exports = {
   theme: {
     fontFamily: {
       body: ['Karla', 'sans-serif']
-    }
+    },
+    colors
   }
 }
