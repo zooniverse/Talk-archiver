979778f79d8bd74f261914c70c0492ce5da28528
Jim O'Donnell <jim@zooniverse.org>
2020-05-12 08:03:40 +0100

    Read subjects file Read subject IDs from a CSV file, then make batched requests for the full subjects.

--- src/api/_data/subjects.js
+++ src/api/_data/subjects.js
@@ -1,73 +1,36 @@
+const csv = require('fast-csv');
+const fs = require('fs');
+const path = require('path');
+
 const API = require('../../helpers/api');
 const store = require('../../helpers/store');
-const fetchUserCollections = require('./userCollections');
 
-async function fetchPage(name, page) {
-  const data = await API.get(`users/${name}?type=my_collections&page=${page}`);
-  return data;
-}
+const subjectURLs = [];
 
-async function listCollections(user_name) {
-  let userCollections = [];
-  for (let page = 2; page <= 100; page++) {
-    const { my_collections } = await fetchPage(user_name, page);
-    if (my_collections.length === 0) {
-      return userCollections
-    }
-    userCollections = userCollections.concat(my_collections);
-  }
-  return userCollections
-}
-
-async function subjectUsers(discussion) {
-  const userURLs = discussion.comments
-    .filter(comment => !store.users[comment.user_name])
-    .map(comment => `users/${comment.user_name}`);
-  const users = await API.batchedGet(userURLs);
-
-  for (user of users) {
-    if (!store.users[user.name]) {
-      if (user.name) {
-        const userCollections = await listCollections(user.name);
-        user.my_collections && user.my_collections.push(...userCollections);
-        store.users[user.name] = user;
-        for (collection of user.my_collections) {
-          if (!store.userCollections[collection.zooniverse_id]) {
-            const userCollection = await API.get(`collections/${collection.zooniverse_id}`)
-            store.userCollections[collection.zooniverse_id] = userCollection;
-          }
-        }
-      } else {
-        console.log(`*** Invalid response for user ${user.name}`)
-      }
-    }
-  }
-}
-
-async function collectionSubjects(collection) {
-  const subjectURLs = collection.subjects
-    .filter(subject => !store.subjects[subject.zooniverse_id])
-    .map(subject => `subjects/${subject.zooniverse_id}`);
-  const fullSubjects = await API.batchedGet(subjectURLs);
-
-  for (fullSubject of fullSubjects) {
-    if (!store.subjects[fullSubject.zooniverse_id]) {
-      await subjectUsers(fullSubject.discussion);
-      store.subjects[fullSubject.zooniverse_id] = fullSubject;
-    }
-  }
+function parseRow({ ouroboros_subject_id }) {
+  const subjectURL = `subjects/${ouroboros_subject_id}`;
+  subjectURLs.push(subjectURL);
 }
 
 module.exports = async function fetchSubjects() {
-  const collections = await fetchUserCollections();
+  const parseData = new Promise((resolve, reject) => {
+    fs.createReadStream(path.resolve(__dirname, '../../../.data', 'illustratedlife_subject_ids.csv'))
+      .pipe(csv.parse({ headers: true }))
+      .on('error', error => reject(error))
+      .on('data', parseRow)
+      .on('end', (rowCount) => {
+        resolve(subjectURLs);
+        console.log(`Parsed ${rowCount} subjects`)
+      });
+  })
 
-  for (collection of Object.values(collections)) {
-    await collectionSubjects(collection);
-  }
+  const urls = await parseData;
+  const subjects = await API.batchedGet(urls);
 
-  for (collection of Object.values(store.userCollections)) {
-    await collectionSubjects(collection);
+  for (subject of subjects) {
+    store.subjects[subject.zooniverse_id] = subject;
   }
 
   return store.subjects;
 }
+
