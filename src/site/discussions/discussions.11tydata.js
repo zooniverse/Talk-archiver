00a7f5f87f32645b63d7cd7dfb2c80041bcf035f
Jim O'Donnell <jim@zooniverse.org>
2020-05-26 11:57:45 +0100

    Build boards and discussions Build board and discussion pages in their own build.

--- src/site/discussions/discussions.11tydata.js
+++
@@ -1,9 +0,0 @@
-const awaitBoards = require('../../helpers/boards');
-const store = require('../../helpers/store');
-
-module.exports = async function fetchDiscussions() {
-  console.log('building discussions');
-  await awaitBoards;
-  const { discussions } = store;
-  return { discussions };
-}
