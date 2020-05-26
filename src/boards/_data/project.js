9b4eadece01522136afd8d2f522a2322d2cdd082
Jim O'Donnell <jim@zooniverse.org>
2020-05-26 13:34:20 +0100

    Fix project data file

--- src/boards/_data/project.js
+++ src/boards/_data/project.js
@@ -3,5 +3,5 @@ const awaitProject = require('../../helpers/project');
 module.exports = async function () {
   console.log('getting project details');
   const project = await awaitProject;
-  return { project };
+  return project;
 }
