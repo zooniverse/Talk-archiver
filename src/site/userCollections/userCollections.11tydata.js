926b887ea46de5cbee17cdc7d1d9927a065e9647
Jim O'Donnell <jim@zooniverse.org>
2020-05-26 17:21:25 +0100

    Remove discussions store from collections Read collection discussions from the data export files, via the discussionComments helper.

--- src/site/userCollections/userCollections.11tydata.js
+++ src/site/userCollections/userCollections.11tydata.js
@@ -1,8 +1,12 @@
 const awaitCollections = require('../../helpers/collections');
-const store = require('../../helpers/store');
+const discussionComments = require('../../helpers/discussionComments');
 
 module.exports = async function () {
+  const { boards } = await discussionComments;
   const userCollections = await awaitCollections;
-  const { discussions } = store;
+  const discussions = {};
+  for (boardDiscussion of boards) {
+    discussions[boardDiscussion.zooniverse_id] = boardDiscussion;
+  }
   return { discussions, userCollections };
 }
