5b44ee843872d23b8687d19e514c85c573d5432d
Jim O'Donnell <jim@zooniverse.org>
2020-06-05 10:11:18 +0100

    Normalise collection links Add an optional attribution after the title. Remove the collection ID.

--- src/components/collectionSummary.js
+++ src/components/collectionSummary.js
@@ -14,12 +14,13 @@ module.exports = function collectionSummary(collection, limit, className='') {
   const subjects = collection.subjects ? collection.subjects.slice(0, limit) : [];
   const zooniverse_id = collection.zooniverse_id || collection._id;
   const title = collection.title || collection.name;
-  const owner = collection.user_name ? `${collection.user_name}:` : '';
+  const attribution = collection.user_name ? `by ${collection.user_name}` : '';
   return `
     <p>
       <a href="/collections/${ zooniverse_id }">
-        ${ owner || zooniverse_id } ${ title }
+        ${ title } ${ attribution }
       </a>
+      (${ collection.subjects.length }) subjects.
     </p>
     <ul class="collection ${className}">
       ${subjects.map(collectionSubject).join('\n')}
