24138f727d12e2b5083caf58ee5e99761b38f726
Jim O'Donnell <jim@zooniverse.org>
2020-06-05 10:31:34 +0100

    Add collection size to summaries

--- src/components/collectionSummary.js
+++ src/components/collectionSummary.js
@@ -14,13 +14,12 @@ module.exports = function collectionSummary(collection, limit, className='') {
   const subjects = collection.subjects ? collection.subjects.slice(0, limit) : [];
   const zooniverse_id = collection.zooniverse_id || collection._id;
   const title = collection.title || collection.name;
-  const attribution = collection.user_name ? `by ${collection.user_name}` : '';
+  const attribution = collection.user_name ? ` by ${collection.user_name}` : '';
+  const plural = collection.subjects && collection.subjects.length > 1 ? 's' : '';
+  const size = collection.subjects && collection.subjects.length ? `(${ collection.subjects.length } subject${ plural })` : '';
   return `
     <p>
-      <a href="/collections/${ zooniverse_id }">
-        ${ title } ${ attribution }
-      </a>
-      (${ collection.subjects.length }) subjects.
+      <a href="/collections/${ zooniverse_id }">${ title }${ attribution }</a> ${ size }
     </p>
     <ul class="collection ${className}">
       ${subjects.map(collectionSubject).join('\n')}
