596d4bfd2736cf2ef8f8d2d54896254ecd3fd38f
Jim O'Donnell <jim@zooniverse.org>
2020-06-16 11:54:02 +0100

    Redirect old hash fragments (#45)
    Redirect old hash fragments for subjects, collections, users and tags to the new page URLs.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
 // console.log("Elevenpack javascript is loaded");
+import slug from '../helpers/slug';
+
+function rewriteHashes(hash) {
+  const paths = hash.split('/');
+  switch (paths[1]) {
+    case "subjects":
+    case "collections": {
+      return `/${paths[1]}/${paths[2]}/`;
+    }
+    case "users": {
+      return `/${paths[1]}/${slug(paths[2])}/`;
+    }
+  }
+  /*
+    tag fragments are of the form
+    #/search?tags[sometag]=true
+  */
+  if (paths[1].startsWith('search?tags')) {
+    const search = paths[1].split('?');
+    const params = search[1];
+    const tagMatch = /tags\[(\w+)\]=true/;
+    const matches = params.match(tagMatch);
+    const tag = matches[1];
+    return tag ? `/tags/${tag}/` : '';
+  }
+
+  return '';
+}
+
+function onLoad() {
+  const { hash } = window.location;
+  if (hash.startsWith('#/')) {
+    const redirectPath = rewriteHashes(hash);
+    if (redirectPath.length) {
+      window.location.replace(`${window.location.origin}${redirectPath}`);
+    }
+  }
+}
+
+window.addEventListener('load', onLoad);
+
