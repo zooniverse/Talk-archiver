f4d4fc0ffc0c6cac54694766c36ea817894047ce
Jim O'Donnell <jim@zooniverse.org>
2020-06-19 10:39:25 +0100

    Verify HTML output Extend the verification script to count HTML files too. Discussions are subdirectories of boards, so have to be counted by looping through the board directories.

--- verify.js
+++ verify.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const manifest = require('./dist/manifest/build');
 
-const source = path.resolve(__dirname, './dist/api');
-const dirs = fs.readdirSync(source, { withFileTypes: true })
+console.log(`Verifying JSON output.`);
+let source = path.resolve(__dirname, './dist/api');
+let dirs = fs.readdirSync(source, { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
 
 dirs.forEach(dir => {
@@ -17,3 +18,35 @@ dirs.forEach(dir => {
     console.error(logSymbols.error, dir.name, files.length, '(', manifest[dir.name], ')');
   }
 });
+
+console.log(`Verifying HTML output.`);
+source = path.resolve(__dirname, './dist');
+dirs = fs.readdirSync(source, { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory() && !!manifest[dirent.name]);
+
+dirs.forEach(dir => {
+  const dirPath = path.resolve(source, dir.name);
+  const files = fs.readdirSync(dirPath, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+  if (files.length === manifest[dir.name]) {
+    console.log(logSymbols.success, dir.name, files.length)
+  } else {
+    console.error(logSymbols.error, dir.name, files.length, '(', manifest[dir.name], ')');
+  }
+});
+
+// discussions are subdirectories of boards
+let discussionsCount = 0;
+source = path.resolve(__dirname, './dist/boards');
+const boards = fs.readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+
+boards.forEach(board => {
+  const discussionsPath = path.resolve(source, board.name, 'discussions');
+  const discussions = fs.readdirSync(discussionsPath, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+  discussionsCount = discussionsCount + discussions.length;
+});
+
+if (discussionsCount === manifest.discussions) {
+  console.log(logSymbols.success, 'discussions', discussionsCount);
+} else {
+  console.error(logSymbols.error, 'discussions', discussionsCount, '(', manifest.discussions, ')');
+}
