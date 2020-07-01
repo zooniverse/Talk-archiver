b6200dd1e7aec7bf2f28db1f3fa7be110c09a14c
Jim O'Donnell <jim@zooniverse.org>
2020-07-01 11:23:47 +0100

    subjectLocation helper function Add the subjectLocation helper function to og:image URLs and the subjectImage component: - Use the first URL if location is an array. - Replace the zooniverse-static S3 domain with static.zooniverse.org. - Replace http:// with https://.

--- src/components/subjectImage.js
+++ src/components/subjectImage.js
@@ -1,13 +1,11 @@
+const subjectLocation = require('../helpers/subjectLocation')
+
 module.exports = function subjectImage(subject, size='standard', className) {
   try {
-    let url = subject.location.standard;
-    if ( Array.isArray(url) ) {
-      url = url[0];
-    }
-    url = url.replace('http://', 'https://');
+    let url = subjectLocation(subject.location.standard);
     const passThrough = (size === 'standard') || url.endsWith('.png');
     const zooniverseID = subject.zooniverse_id || subject._id;
-    const staticRoot = 'zooniverse-static.s3.amazonaws.com/';
+    const staticRoot = 'static.zooniverse.org';
     const thumbnailPath = url.replace('https://', '');
     const src =  passThrough ? url : `https://thumbnails.zooniverse.org/150x150/${thumbnailPath.replace(staticRoot, '')}`;
     const classAttr = className ? `class=${className}` : '';
