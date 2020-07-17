c2979dff30e6618206cf7ffff52870335371bb23
Jim O'Donnell <jim@zooniverse.org>
2020-07-17 10:08:44 +0100

    Log subject hosts We want to check for subjects that are using S3 URLs, which will break as we migrate away from AWS to Azure storage. Get the host from each subject URL and log the total count of subjects per host used.

---
+++ src/subjects:api/manifest/locations.11tydata.js
@@ -0,0 +1,19 @@
+const awaitSubjects = require('../../helpers/subjects');
+const subjectLocation = require('../../helpers/subjectLocation');
+
+module.exports = async function () {
+  const subjects = await awaitSubjects;
+  const hosts = {};
+
+  Object.values(subjects).forEach(function (subject) {
+    const location = subjectLocation(subject.location.standard);
+    const url = new URL(location);
+    if (hosts[url.host]) {
+      hosts[url.host] = hosts[url.host] + 1;
+    } else {
+      hosts[url.host] = 1;
+    }
+  })
+
+  return { hosts };
+}
