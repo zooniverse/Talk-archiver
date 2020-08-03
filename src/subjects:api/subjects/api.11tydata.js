b5eb813082970ea249111ec44a3f28ed95160e79
Jim O'Donnell <jim@zooniverse.org>
2020-08-03 14:33:40 +0100

    Subject pages: load all comments (#71)
    For each subject page (including collection listings), load all subject comments from the discussionComments object.

--- src/subjects:api/subjects/api.11tydata.js
+++ src/subjects:api/subjects/api.11tydata.js
@@ -1,7 +1,14 @@
 const API = require('../../helpers/api');
+const discussionComments = require('../../helpers/discussionComments');
 
 module.exports = {
   eleventyComputed: {
-    subject: async data => await API.get(data.subjectURL)
+    subject: async data => await API.get(data.subjectURL),
+    comments: async data => {
+      const { subjects } = await discussionComments;
+      const [ discussion ] = subjects.filter(discussion => discussion.focus._id === data.subject.zooniverse_id);
+      data.subject.discussion = discussion || { comments: [] };
+      return discussion && discussion.comments;
+    }
   }
 }
