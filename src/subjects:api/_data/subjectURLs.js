f874f29c5a79c3b1efca6dc4bbcddcb87da4f9cd
Jim O'Donnell <jim@zooniverse.org>
2020-08-05 15:24:37 +0100

    Dynamic subject batch size Set the subject batch size based on the total number of subjects.

--- src/subjects:api/_data/subjectURLs.js
+++ src/subjects:api/_data/subjectURLs.js
@@ -3,11 +3,11 @@ const awaitSubjectURLs = require('../../helpers/subjectURLs');
 const args = process.argv.slice(2);
 const batch = args[args.length - 2] - 1;
 
-const start = batch * 28000;
-const end = start + 28000;
-
 module.exports = async function () {
   const subjectURLs = await awaitSubjectURLs;
+  const batchSize = Math.ceil( subjectURLs.length / 10 );
+  const start = batch * batchSize;
+  const end = start + batchSize;
   console.log('Processing subjects', start, 'to', end);
   return subjectURLs.slice(start, end);
 }
