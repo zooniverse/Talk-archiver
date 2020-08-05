0bd014e6e12c6363ee3790760d465c9924885430
Jim O'Donnell <jim@zooniverse.org>
2020-08-05 11:12:59 +0100

    Build user comments in batches Run build:users:comments in 10 batches, with a limit of 5,000 users on each run.

--- src/users:comments/_data/users.js
+++ src/users:comments/_data/users.js
@@ -1,6 +1,14 @@
 const awaitUsers = require('../../helpers/users');
 
+const args = process.argv.slice(2);
+const batch = args[args.length - 2] - 1;
+
+const start = batch * 5000;
+const end = start + 5000;
+
 module.exports = async function () {
   const users = await awaitUsers;
-  return users;
+  console.log('Processing users', start, 'to', end);
+  const usersArray = Object.values(users).slice(start, end);
+  return usersArray;
 }
