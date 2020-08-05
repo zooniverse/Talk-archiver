ad5ccec655d51b6dc7f2efeab1bf46e1df135cbb
Jim O'Donnell <jim@zooniverse.org>
2020-08-05 14:59:48 +0100

    Build subjects:api in batches Run build:subjects:api in 10 batches, with a limit of 28,000 subjects on each run.

--- src/subjects:api/_data/subjectURLs.js
+++ src/subjects:api/_data/subjectURLs.js
@@ -1,6 +1,13 @@
 const awaitSubjectURLs = require('../../helpers/subjectURLs');
 
+const args = process.argv.slice(2);
+const batch = args[args.length - 2] - 1;
+
+const start = batch * 28000;
+const end = start + 28000;
+
 module.exports = async function () {
   const subjectURLs = await awaitSubjectURLs;
-  return subjectURLs;
+  console.log('Processing subjects', start, 'to', end);
+  return subjectURLs.slice(start, end);
 }
